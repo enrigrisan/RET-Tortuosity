7c9576ebb73dfa79145e6863351b920b29a6a809
enrigrisan <107041311+enrigrisan@users.noreply.github.com>
2022-06-14 12:53:09 +0100

    Add files via upload

---
+++ testTORTarteries.m
@@ -0,0 +1,35 @@
+load('./data/Artery_Data.mat')
+
+%% Match name ordering in the vessel structure with name ordering for manual grading
+for ct=1:30,
+    fname=fn{ct};
+    n=findstr(fname,'-');
+    fstr{ct}=fname(n+1:end-4);
+end;
+
+[fsort,namesort]=sort(fstr);
+
+%% Manual tortuosity ranking
+clinsort_a=[12,26,2,24,8,15,29,7,28,10,14,22,27,6,20,3,11,5,1,16,21,25,13,19,17,18,23,4,9,30];
+clinsort=clinsort_a;
+
+%% Evaluating tortuosity indeces
+tortpar.k0    = 10;
+tortpar.h     =  0.03;
+tortpar.passo = 0.1;
+tortpar.lmin  = 1;
+
+t=[];
+for ct=1:length(fsort),
+    [tort,x,y,k,indici]=TORTsegind(segs(namesort(ct)).ppx,segs(namesort(ct)).ppy,tortpar,dbf);
+    [t(ct,:),indici]=TORTind(x,y,k,tortpar.lmin,tortpar.h,tortpar.k0,tortpar.passo,dbf);
+end;
+
+%% Evaluate correlation between each tortuosity metric and the manual ranking
+tsort=zeros(size(t));
+for ct=1:size(t,2)
+    temp=t(:,ct);
+    [tempsort,tortidx]=sort(temp);
+    c(ct)=corr(sort(clinsort)',clinsort(tortidx)','type','Spearman');
+end;
+    
---
+++ testTORTveins.m
@@ -0,0 +1,35 @@
+load('./data/Vein_Data.mat')
+
+%% Match name ordering in the vessel structure with name ordering for manual grading
+for ct=1:30,
+    fname=fn{ct};
+    n=findstr(fname,'-');
+    fstr{ct}=fname(n+1:end-4);
+end;
+
+[fsort,namesort]=sort(fstr);
+
+%% Manual tortuosity ranking
+clinsort_v=[10,4,1,19,8,23,22,13,9,17,21,11,12,20,14,7,5,27,26,25,15,3,24,16,18,28,29,2,6,30];
+clinsort=clinsort_v;
+
+%% Evaluating tortuosity indeces
+tortpar.k0    = 10;
+tortpar.h     =  0.03;
+tortpar.passo = 0.1;
+tortpar.lmin  = 1;
+
+t=[];
+for ct=1:length(fsort),
+    [tort,x,y,k,indici]=TORTsegind(segs(namesort(ct)).ppx,segs(namesort(ct)).ppy,tortpar,dbf);
+    [t(ct,:),indici]=TORTind(x,y,k,tortpar.lmin,tortpar.h,tortpar.k0,tortpar.passo,dbf);
+end;
+
+%% Evaluate correlation between each tortuosity metric and the manual ranking
+tsort=zeros(size(t));
+for ct=1:size(t,2)
+    temp=t(:,ct);
+    [tempsort,tortidx]=sort(temp);
+    c(ct)=corr(sort(clinsort)',clinsort(tortidx)','type','Spearman');
+end;
+    
